9d47c298b9521dbb8d02cac9f33572c9270d1a69
Jonathan King <jking@fearless.tech>
2017-10-20 09:22:25 -0400

    DT-2273 cleans up the bash script

--- scripts/create.sh
+++ scripts/create.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
-export S3Bucket=sba-tools
-export S3Folder=size-standards
-export ZipFileName=size-standards.zip
-export OpenApiSpecFilename=api-specification.yaml
-export CloudFormationTemplateName=template.yaml
+#!/bin/bash -e
+export S3BUCKET=sba-tools
+export S3FOLDER=size-standards
+export ZIP_FILENAME=size-standards.zip
+export OPEN_API_SPEC_FILENAME=api-specification.yaml
+export CLOUD_FORMATION_TEMPLATE_NAME=template.yaml
 
 # create code bundle
 echo "Creating and Uploading Code Bundle"
@@ -13,22 +13,22 @@ cp src/* build/
 cd build
 npm i --prod --silent
 rm package.json
-zip -r -q ../$ZipFileName .
+zip -r -q ../${ZIP_FILENAME} .
 cd ..
-aws s3 cp $ZipFileName s3://$S3Bucket/$S3Folder/$ZipFileName
-rm $ZipFileName
+aws s3 cp ${ZIP_FILENAME} s3://${S3BUCKET}/${S3FOLDER}/${ZIP_FILENAME}
+rm ${ZIP_FILENAME}
 rm -rf build
 
 # upload the api specification
 echo "Uploading API Specification"
-aws s3 cp configuration/$OpenApiSpecFilename s3://$S3Bucket/$S3Folder/$OpenApiSpecFilename
+aws s3 cp configuration/${OPEN_API_SPEC_FILENAME} s3://${S3BUCKET}/${S3FOLDER}/${OPEN_API_SPEC_FILENAME}
 
 echo "Uploading CloudFormation Template"
-aws s3 cp configuration/$CloudFormationTemplateName s3://$S3Bucket/$S3Folder/$CloudFormationTemplateName
+aws s3 cp configuration/${CLOUD_FORMATION_TEMPLATE_NAME} s3://${S3BUCKET}/${S3FOLDER}/${CLOUD_FORMATION_TEMPLATE_NAME}
 
 echo "Creating Stack"
-aws cloudformation create-change-set --stack-name SizeStandardsToolStack --change-set-name SizeStandardsToolChangeSet --change-set-type CREATE --template-url https://s3.amazonaws.com/$S3Bucket/$S3Folder/$CloudFormationTemplateName --capabilities "CAPABILITY_IAM"
-sleep 5
+aws cloudformation create-change-set --stack-name SizeStandardsToolStack --change-set-name SizeStandardsToolChangeSet --change-set-type CREATE --template-url https://s3.amazonaws.com/${S3BUCKET}/${S3FOLDER}/${CLOUD_FORMATION_TEMPLATE_NAME} --capabilities "CAPABILITY_IAM"
+aws cloudformation change-set-create-complete --stack-name SizeStandardsToolStack --change-set-name SizeStandardsToolChangeSet
 aws cloudformation execute-change-set --stack-name SizeStandardsToolStack  --change-set-name SizeStandardsToolChangeSet
 
 echo "Done"
